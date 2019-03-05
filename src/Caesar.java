875a2eb7a410ef5fb0c167fe277d42c5100d7010
mark <marrk.rafalko@gmail.com>
2019-03-05 09:00:05 +0100

    test commit

--- src/Caesar.java
+++ src/Caesar.java
@@ -6,6 +6,7 @@ public class Caesar {
     private int n = alphabet.length();
     private int k0 = 3;
     private int k1 = 7;
+    private int xxx;
 
     public static void main(String[] args) {
 
